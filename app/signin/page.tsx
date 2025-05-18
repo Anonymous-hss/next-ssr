33499cb69cc4a5e2eac7447d0958837d67cf56c0
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-05-18 21:45:15 +0530

    first commit

---
+++ app/signin/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+export default function Signin() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="border p-2">
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button onClick={() => {}}>Sign In</button>
+      </div>
+    </div>
+  );
+}
---
+++ app/user/page.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default async function User() {
+  const response = await axios.get("http://localhost:3000/api/v1/user/details");
+  const data = response.data;
+
+  return <div>User Page {data.user}</div>;
+}
